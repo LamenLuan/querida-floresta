28ab7d46e3aa7f85e5bd43f6d7813a91cfa68682
LamenLuan <luan.leme.s35@gmail.com>
2021-06-02 19:14:50 -0300

    Adding start and quit functionalities for main menu

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Called by Button (btQuit)
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void startGame()
+    {
+        SceneManager.LoadScene("Scene 1");
+    }
+}
