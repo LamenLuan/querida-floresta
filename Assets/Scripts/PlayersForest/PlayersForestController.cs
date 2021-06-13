8fcc857f450d08735768ca76eb763dba8b9f3935
LamenLuan <luan.leme.s35@gmail.com>
2021-06-13 17:52:28 -0300

    Player now need to click on screen to unlock reward

--- Assets/Scripts/PlayersForest/PlayersForestController.cs
+++ Assets/Scripts/PlayersForest/PlayersForestController.cs
@@ -4,9 +4,10 @@ using UnityEngine.UI;
 
 public class PlayersForestController : MonoBehaviour
 {
-    [SerializeField] private GameObject reward1, reward2, reward3, canvas;
+    [SerializeField] private GameObject reward1Obj, reward2Obj, reward3Obj,
+    canvasObj, imgLevelSignObj;
     [SerializeField] private AudioController audioController;
-
+    private string functionToInvoke;
     private GameObject newReward;
 
     public void loadMainMenu() // Invoked in Start()
@@ -26,51 +27,69 @@ public class PlayersForestController : MonoBehaviour
 
     private void setRewardsActive()
     {
-        GameObject[] rewards = {reward1, reward2, reward3};
+        GameObject[] rewards = {reward1Obj, reward2Obj, reward3Obj};
 
         for (int i = 0; i < AplicationModel.scenesCompleted; i++)
             rewards[i].SetActive(true);
     }
 
-    private void showReward() // Invoked in Start()
+    private void showReward()
     {
+        // Removing the visual effect to show new reward
         for (int i = 0; i < newReward.transform.childCount; ++i)
         {
             newReward.transform.GetChild(i).GetComponent<Image>().color =
                 new Vector4(1f, 1f, 1f, 1f);
         }
         audioController.sceneCompletedSound();
+        if(functionToInvoke != null) Invoke(functionToInvoke, 8f);
+    }
+
+    private void addButtonInCanvas() // Invoked in Start()
+    {
+        Button canvasButton = canvasObj.AddComponent<Button>();
+        canvasButton.transition = Selectable.Transition.None;
+        canvasButton.onClick.AddListener( () => {
+            showReward();
+            imgLevelSignObj.SetActive(false);
+            canvasButton.onClick.RemoveAllListeners();
+        });
+    }
+
+    private void setLockedEffectOnNewReward()
+    {
+        for (int i = 0; i < newReward.transform.childCount; ++i)
+        {
+            newReward.transform.GetChild(i).GetComponent<Image>().color =
+                new Vector4(0f, 0f, 0f, 1f);
+        }
     }
 
     void Start() // Start is called before the first frame update
     {
-        string functionToInvoke = null;
-        
+        functionToInvoke = null;
+
         if(AplicationModel.isForestInTemporaryMode)
         {
             switch (AplicationModel.scenesCompleted)
             {
                 case 1:
-                    newReward = reward1;
+                    newReward = reward1Obj;
                     functionToInvoke = "loadScene2";
                     break;
                 case 2:
-                    newReward = reward2;
+                    newReward = reward2Obj;
                     functionToInvoke = "loadScene3";
                     break;
                 case 3:
-                    newReward = reward3;
+                    newReward = reward3Obj;
                     break;
             }
 
-            for (int i = 0; i < newReward.transform.childCount; ++i)
-            {
-                newReward.transform.GetChild(i).GetComponent<Image>().color =
-                    new Vector4(0f, 0f, 0f, 1f);
-            }
+            setLockedEffectOnNewReward();
+            imgLevelSignObj.SetActive(true);
+            Invoke("addButtonInCanvas", 1f);
         }
         setRewardsActive();
-        Invoke("showReward", 3f);
-        if(functionToInvoke != null) Invoke(functionToInvoke, 8f);
     }
 }
