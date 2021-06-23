bb6ab4fe4ba7ae59576880263313a49d05730b02
LamenLuan <luan.leme.s35@gmail.com>
2021-06-23 11:29:53 -0300

    Adding color effects to the third question buttons

--- Assets/Scripts/Scene3/CanvasS3Controller.cs
+++ Assets/Scripts/Scene3/CanvasS3Controller.cs
@@ -1,57 +1,70 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class CanvasS3Controller : MonoBehaviour
 {
     [SerializeField] private GameObject icon1BtnObj, icon2BtnObj, icon3BtnObj,
-    word1Obj, word2Obj, backgroundCoverObj;
+    word1Obj, word2Obj, backgroundCoverObj, quitButtonObj;
     [SerializeField] private Sprite btn1Sprite, btn2Sprite, btn3Sprite;
 
-    IEnumerator ResetOutline(Outline outline)
+    IEnumerator ResetEffect(Component component, bool isOutline)
     {
         yield return new WaitForSeconds(2f);
-        try
+        
+        if(isOutline)
         {
-            outline.effectColor = new Color(0.312f, 0.208f, 0f);
+            ( (Outline) component ).effectColor =
+                new Color(0.312f, 0.208f, 0f);
         }
-        catch (System.Exception) {}
+        else ( (Image) component ).color = new Color(1f, 1f, 1f);
     }
 
-    public void setOutline(Button button, bool hit)
+    public void setAnswerEffect(Button button, bool isRightAnswer)
     {
+        Func<Color> getAnswerEffect = () => {
+            return isRightAnswer
+                ? new Color(0f, 1f, 0f)
+                : new Color(1f, 0f, 0f)
+            ;
+        };
+
         Outline outline = button.gameObject.GetComponent<Outline>();
-        
-        outline.effectColor = hit
-            ? new Color(0f, 1f, 0f)
-            : new Color(1f, 0f, 0f)
-        ;
-        
-        StartCoroutine( ResetOutline(outline) );
+
+        if(outline != null)
+        {
+            outline.effectColor = getAnswerEffect();
+            StartCoroutine( ResetEffect(outline, true) );
+        }
+        else {
+            Image image = button.gameObject.GetComponent<Image>();
+            image.color = getAnswerEffect();
+            StartCoroutine( ResetEffect(image, false) );
+        }
     }
 
     // Invoked by activateBackgroundCover()
-    public void deactivateBackgroundCover()
+    public void deactivateBackgroundCover() 
     {
         backgroundCoverObj.SetActive(false);
     }
 
-    public void activateBackgroundCover()
+    // Invoked by the option buttons
+    public void activateBackgroundCover(float timeToDeactivate) 
     {
         backgroundCoverObj.SetActive(true);
-        Invoke("deactivateBackgroundCover", 2f);
-    } 
+        Invoke("deactivateBackgroundCover", timeToDeactivate);
+    }
 
-    // Invoked by scene controller
-    public void changeToQuestionTwo()
+    public void changeToQuestionTwo() // Invoked by Scene3Controller
     {
         icon1BtnObj.GetComponent<Image>().sprite = btn1Sprite;
         icon2BtnObj.GetComponent<Image>().sprite = btn2Sprite;
         icon3BtnObj.GetComponent<Image>().sprite = btn3Sprite;
     }
 
-    // Invoked by scene controller
-    public void changeToQuestionThree()
+    public void changeToQuestionThree() // Invoked by Scene3Controller
     {
         Destroy(icon1BtnObj);
         Destroy(icon2BtnObj);
@@ -61,11 +74,10 @@ public class CanvasS3Controller : MonoBehaviour
         word2Obj.SetActive(true);
     }
 
-    // Invoked by scene controller
-    public void levelIsOver()
+    public void levelIsOver() // Invoked by Scene3Controller
     {
+        Destroy(quitButtonObj);
         Destroy(word1Obj);
         Destroy(word2Obj);
-        Destroy(backgroundCoverObj);
     }
 }
