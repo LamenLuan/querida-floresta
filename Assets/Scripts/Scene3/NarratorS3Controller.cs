a2d7a6916bbfaead4d07d8c80677c77ec644e548
LamenLuan <luan.leme.s35@gmail.com>
2021-06-23 11:30:08 -0300

    Adding narrator audios

---
+++ Assets/Scripts/Scene3/NarratorS3Controller.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class NarratorS3Controller : NarratorController
+{
+    [SerializeField] private CanvasS3Controller canvasController;
+    [SerializeField] private AudioSource introductionAudio, question1Audio,
+    question2Audio, question3Audio, rightAnswerAudio, wrongAnswerAudio,
+    sceneCompletedAudio;
+
+    public AudioSource SceneCompletedAudio
+    {
+        get { return sceneCompletedAudio; }
+    }
+
+    public AudioSource RightAnswerAudio
+    {
+        get { return rightAnswerAudio; }
+    }
+
+    private void playNarratorAudio(AudioSource audio)
+    {
+        canvasController.activateBackgroundCover(audio.clip.length + 1f);
+        playAudio(audio);
+    }
+
+    private void playQuestion1Audio() // Invoked by playIntroductionAudio()
+    {
+        playNarratorAudio(question1Audio);
+    }
+    public void playQuestion2Audio()
+    {
+        playNarratorAudio(question2Audio);
+    }
+
+    public void playQuestion3Audio()
+    {
+        playNarratorAudio(question3Audio);
+    }
+
+    public void playIntroductionAudio()
+    {
+        playNarratorAudio(introductionAudio);
+        Invoke("playQuestion1Audio", introductionAudio.clip.length + 1f);
+    }
+
+    public void playRightAnswerAudio()
+    {
+        playNarratorAudio(rightAnswerAudio);
+    }
+
+    public void playWrongAnswerAudio()
+    {
+        playNarratorAudio(wrongAnswerAudio);
+    }
+
+    public void playSceneCompletedAudio()
+    {
+        playNarratorAudio(sceneCompletedAudio);
+    }
+}
