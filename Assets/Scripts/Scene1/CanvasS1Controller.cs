6cf5221e93e92774781348841a45c18695fb35a9
LamenLuan <luan.leme.s35@gmail.com>
2021-07-06 17:14:36 -0300

    Removing unused functions and operations

--- Assets/Scripts/Scene1/CanvasS1Controller.cs
+++ Assets/Scripts/Scene1/CanvasS1Controller.cs
@@ -6,12 +6,16 @@ public class CanvasS1Controller : MonoBehaviour
     [SerializeField] private GameObject backgroundCoverObj, toucanObj,
     levelTxtObj, nextLevelBtnObj, quitBtnObj, helpBtnObj, startBtnObj,
     tryAgainBtnObj, levelSignObj, mouseObj;
-
     [SerializeField] private Text cloudQuantityTxt;
 
-    public void enableTryAgainButton()
+    public void enableTryAgainButton() // Invoked by Scene1Controller
     {
         tryAgainBtnObj.GetComponent<Button>().interactable = true;
+    }
+
+    public void showNextLevelButton() // Invoked by Scene1Controller
+    {
+        nextLevelBtnObj.SetActive(true);
     }
 
     public void showButtons() // Invoked in Scene1Controller
@@ -26,6 +30,12 @@ public class CanvasS1Controller : MonoBehaviour
         GameObject[] buttons = {startBtnObj, helpBtnObj, quitBtnObj};
 
         foreach (GameObject buttonObj in buttons) buttonObj.SetActive(false);
+    }
+
+    public void disableStartInterface() // Called by Button (btStart)
+    {
+        hideButtons();
+        levelSignObj.SetActive(false);
     }
 
     public void updateLevelTxt(int level)
@@ -49,14 +59,18 @@ public class CanvasS1Controller : MonoBehaviour
         rect.anchorMin = new Vector2(minX, minY);
         rect.anchorMax = new Vector2(maxX, maxY);
     }
+    
+    public void changeQuantityTxt(int quantity)
+    {
+        cloudQuantityTxt.text = quantity.ToString();
+    }
 
     public void resetInterface()
     {
-        GameObject[] objectsToActivate = 
-            {backgroundCoverObj, levelSignObj, levelTxtObj};
+        GameObject[] objectsToActivate = {levelSignObj, levelTxtObj};
 
         GameObject[] objectsToDeactivate = 
-            {tryAgainBtnObj, nextLevelBtnObj, mouseObj};
+            {backgroundCoverObj, tryAgainBtnObj, nextLevelBtnObj, mouseObj};
 
         tryAgainBtnObj.GetComponent<Button>().interactable = false;
         foreach (GameObject item in objectsToDeactivate) item.SetActive(false);
@@ -65,40 +79,10 @@ public class CanvasS1Controller : MonoBehaviour
             -0.000289917f, 0.08061218f, 0.7796173f, -0.7994232f, 0f, 0.319f,
             0.2241268f, 0.61f
         );
-
-        backgroundCoverObj.GetComponent<Image>().color =
-            new Vector4(1f, 1f, 1f, 0f);            
-        cloudQuantityTxt.text = "0";
-
+              
+        changeQuantityTxt(0);
         showButtons();
         foreach (GameObject item in objectsToActivate) item.SetActive(true);
-    }
-
-    public void changeQuantityTxt(int quantity)
-    {
-        cloudQuantityTxt.GetComponent<Text>().text = quantity.ToString();
-    }
-
-    public void showNextLevelButton()
-    {
-        nextLevelBtnObj.SetActive(true);
-    }
-
-    // Called by Button (btStart)
-    public void disableStartInterface()
-    {
-        GameObject[] objects = {backgroundCoverObj, levelSignObj};
-
-        hideButtons();
-        foreach (GameObject item in objects) item.SetActive(false);
-    }
-
-    private void showBlackBackgroundCover()
-    {
-        backgroundCoverObj.GetComponent<Image>().color =
-            new Vector4(0f, 0f, 0f, 0.55f);
-
-        backgroundCoverObj.SetActive(true);
     }
 
     public void changeToTryAgainInterface()
@@ -112,14 +96,13 @@ public class CanvasS1Controller : MonoBehaviour
             0.6632535f, 0.776826f
         );
 
-        showBlackBackgroundCover();
+        backgroundCoverObj.SetActive(true);
         tryAgainBtnObj.SetActive(true);
     }
 
     public void changeToHelpInterface()
     {
         hideButtons();
-        showBlackBackgroundCover();
         levelSignObj.SetActive(false);
         mouseObj.SetActive(true);
     }
