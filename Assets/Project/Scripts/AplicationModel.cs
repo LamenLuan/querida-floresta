a6b32014b2ea27142df70baeadbc35430a1f775e
LamenLuan <luan.leme.s35@gmail.com>
2022-08-10 16:08:47 -0300

    Adding hidden editor mode switch to login witout reading qr code

--- Assets/Project/Scripts/AplicationModel.cs
+++ Assets/Project/Scripts/AplicationModel.cs
@@ -1,23 +1,27 @@
 ﻿public class AplicationModel
 {
-    private static AplicationModel _instace;
+	private static AplicationModel _instace;
 
-    public static AplicationModel Instance {
-        get {
-            if (_instace == null) _instace = new AplicationModel();
-            return _instace;
-        }
-    }
+	public static AplicationModel Instance
+	{
+		get
+		{
+			if (_instace == null) _instace = new AplicationModel();
+			return _instace;
+		}
+	}
 
-    public static bool isForestInTemporaryMode = true, lessClouds;
-    public static bool isFirstTimeScene1 = true, isFirstTimeScene2 = true;
+	public static bool EditorMode = true;
 
-    // Report data
-    public static byte[] SceneAcesses { get; } = new byte[3];
-    public static byte[] Scene1Misses { get; } = new byte[3];
-    public static byte Scene2Misses { get; set; }
-    public static byte[] Scene3Misses { get; } = new byte[3];
-    public static double[] PlayerResponseTime { get; } = new double[3];
+	public static bool isForestInTemporaryMode = true, lessClouds;
+	public static bool isFirstTimeScene1 = true, isFirstTimeScene2 = true;
 
-    private AplicationModel() {}
+	// Report data
+	public static byte[] SceneAcesses { get; } = new byte[3];
+	public static byte[] Scene1Misses { get; } = new byte[3];
+	public static byte Scene2Misses { get; set; }
+	public static byte[] Scene3Misses { get; } = new byte[3];
+	public static double[] PlayerResponseTime { get; } = new double[3];
+
+	private AplicationModel() { }
 }
--- Assets/Project/Scripts/AuthMenu/AuthController.cs
+++ Assets/Project/Scripts/AuthMenu/AuthController.cs
@@ -1,62 +1,92 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class AuthController : MonoBehaviour
 {
-    [SerializeField] private SceneLoader sceneLoader;
-    [SerializeField] private GoogleSheetsController sheetsController;
-    [SerializeField] private WebCamController webCamController;
-    [SerializeField] private GameObject buttonsObj, noConnectionObj, webCamObj;
-    [SerializeField] private Text quitButtonTxt;
-    private bool readQr;
+	[SerializeField] private SceneLoader sceneLoader;
+	[SerializeField] private GoogleSheetsController sheetsController;
+	[SerializeField] private WebCamController webCamController;
+	[SerializeField] private GameObject buttonsObj, noConnectionObj, webCamObj;
+	[SerializeField] private Text quitButtonTxt;
+	[SerializeField] private Toggle editorTg;
+	private bool readQr;
 
-    void Start()
-    {
-        try {
-            sheetsController.StartSheets();
-        }
-        catch(System.Net.Http.HttpRequestException) {
-            ErrorMode("Erro de conexão com a rede");
-        }
-        catch(System.Exception) {
-            ErrorMode("Um erro inesperado aconteceu");
-        }
-    }
-    
-    public void ReadQrMode()
-    {
-        readQr = true;
-        buttonsObj.SetActive(false);
-        webCamObj.SetActive(true);
-        quitButtonTxt.text = "VOLTAR";
-        webCamController.StartWebCam();
-    }
+	void Start()
+	{
+		editorTg.SetIsOnWithoutNotify(AplicationModel.EditorMode);
+		if (EditorApplication.isPlaying) editorTg.gameObject.SetActive(true);
 
-    public void ErrorMode(string msg)
-    {
-        buttonsObj.SetActive(false);
-        webCamObj.SetActive(false);
-        Text text = noConnectionObj.transform.GetComponentInChildren<Text>();
-        text.text = msg;
-        noConnectionObj.SetActive(true);
-    }
+		try
+		{
+			sheetsController.StartSheets();
+		}
+		catch (System.Net.Http.HttpRequestException)
+		{
+			ErrorMode("Erro de conexão com a rede");
+		}
+		catch (System.Exception)
+		{
+			ErrorMode("Um erro inesperado aconteceu");
+		}
+	}
 
-    public bool LoadPlayer(string id)
-    {
-        var data = sheetsController.FindEntry(id);
-        if (data == null) return false;
-        Player.Instance.LoadData(data);
+	public void ReadQrMode()
+	{
+		if (editorTg.gameObject.activeSelf && AplicationModel.EditorMode)
+		{
+			LoadEditorPlayer();
+			return;
+		}
 
-        sceneLoader.Invoke("loadMainMenu", 2.0f);
-        return true;
-    }
+		readQr = true;
+		buttonsObj.SetActive(false);
+		webCamObj.SetActive(true);
+		quitButtonTxt.text = "VOLTAR";
+		webCamController.StartWebCam();
+	}
 
-    public void QuitButtonAction()
-    {
-        if(readQr) {
-            webCamController.StopCam();
-            sceneLoader.loadAuthMenu();
-        }
-        else sceneLoader.quitGame();
-    }
+	public void SetEditorMode(bool onValueChanged) // Called by Toggle (tgEditor)
+	{
+		AplicationModel.EditorMode = !AplicationModel.EditorMode;
+	}
+
+	public void ErrorMode(string msg)
+	{
+		buttonsObj.SetActive(false);
+		webCamObj.SetActive(false);
+		Text text = noConnectionObj.transform.GetComponentInChildren<Text>();
+		text.text = msg;
+		noConnectionObj.SetActive(true);
+	}
+
+	private bool LoadPlayerData(string id)
+	{
+		var data = sheetsController.FindEntry(id);
+		if (data == null) return false;
+		Player.Instance.LoadData(data);
+		return true;
+	}
+
+	public void LoadEditorPlayer()
+	{
+		if (LoadPlayerData("1")) sceneLoader.loadMainMenu();
+	}
+
+	public bool LoadPlayer(string id)
+	{
+		var dataLoaded = LoadPlayerData(id);
+		if (dataLoaded) sceneLoader.Invoke("loadMainMenu", 2.0f);
+		return dataLoaded;
+	}
+
+	public void QuitButtonAction()
+	{
+		if (readQr)
+		{
+			webCamController.StopCam();
+			sceneLoader.loadAuthMenu();
+		}
+		else sceneLoader.quitGame();
+	}
 }
